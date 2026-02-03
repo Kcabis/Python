0e1e9678367bdf7675a0971074547a25d82966bc
Kcabis <165569849+Kcabis@users.noreply.github.com>
2026-02-03 13:52:51 +0545

    Add files via upload

---
+++ Conditional/even.py
@@ -0,0 +1,18 @@
+x=int(input("please enter a number:"))
+if x==0:
+    print("The number is zero")
+elif x%2==0:
+    print("The number is even")
+else:
+    print("The number is odd")
+
+y=int(input("please enter another number:"))
+if y%2==0:
+    print("The number is even")
+elif y==0:
+    print("The number is zero")
+else:
+    print("The number is odd")
+
+
+ 
---
+++ Conditional/if_elif.py
@@ -0,0 +1,24 @@
+marks = 75
+
+if marks >= 80:
+    print("Grade A")
+
+if marks >= 60:
+    print("Grade B")
+
+if marks >= 40:
+    print("Grade C")
+ #output will be  Grade B, Grade C
+
+#on use of elif else
+marks = 75
+
+if marks >= 80:
+    print("Grade A")
+elif marks >= 60:
+    print("Grade B")
+elif marks >= 40:
+    print("Grade C")
+else:
+    print("Fail")
+#output will be Grade B
